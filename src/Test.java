b38cc36b6ef4d6e81ee1d24c91b469ac34baecd1
Yuliya <yuliaimnadze@yandex.ru>
2022-04-30 19:54:49 +0300

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,21 @@
+public class Test {
+    class Coffee {
+        String coffeeName;
+        String clientName;
+        boolean isCinnamon;
+
+        public Coffee(String coffeeName, String clientName, boolean isCinnamon) {
+            this.clientName = coffeeName;
+            this.clientName = clientName;
+            this.isCinnamon = isCinnamon;
+        }
+    }
+
+    public class Practicum {
+        public void main(String[] args) {
+            Coffee coffee = new Coffee("Каппучино", "Маша", true);
+
+        }
+    }
+
+}
